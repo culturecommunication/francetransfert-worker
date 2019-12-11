b233e72cdf8ebeb6f92a4d96e0bcffe89096fb79
TAHTAH Hussein <EXT_HTA37@open-groupe.com>
2019-12-11 20:44:56 +0100

    committing addition of workers jedis and cron created

---
+++ src/main/java/workers/MailSender.java
@@ -0,0 +1,17 @@
+package workers;
+
+public class MailSender {
+	
+	
+	public void sendDeletionNotificationMail(String emailAddress) {
+		
+	}
+	
+	public void sendUploadSuccessNotificationMail(String emailAddress) {
+		
+	}
+	
+	public void sendDownloadLinkMail(String emailAddress) {
+		
+	}
+}
---
+++ src/main/java/workers/ZipWorker.java
@@ -0,0 +1,53 @@
+package workers;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import com.opengroup.mc.francetransfert.api.francetransfert_metaload_api.RedisManager;
+
+import redis.clients.jedis.JedisPubSub;
+
+
+public class ZipWorker implements Runnable{
+	
+    RedisManager manager;
+    JedisPubSub jedisPubSub;
+    public ZipWorker(RedisManager manager) {
+    	this.manager = manager;
+    	this.jedisPubSub = createPubSub();
+    }
+
+	private JedisPubSub createPubSub() {
+		JedisPubSub jedisPubSub = null;
+		try {
+	    	jedisPubSub = new JedisPubSub() {
+	    		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+	            @Override
+	            public void onMessage(String channel, String message) {
+	            	ZipWorkerTask task = new ZipWorkerTask();
+	            	executor.execute(task);
+	                System.out.println("Channel " + channel + " has sent a message : " + message );
+	            }
+	                     
+	            @Override
+	            public void onSubscribe(String channel, int subscribedChannels) {
+	                System.out.println("Client is Subscribed to channel : "+ channel);
+	            }
+	                 
+	            @Override
+	            public void onUnsubscribe(String channel, int subscribedChannels) {
+	            }
+	                 
+	        };
+	                 
+	        } catch(Exception ex) {         
+	            System.out.println("Exception : " + ex.getMessage());           
+	        }
+		return jedisPubSub;
+	}
+
+	@Override
+	public void run() {
+		manager.subscribe(jedisPubSub, "zip-worker-queue");
+	}
+}
