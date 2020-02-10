2c343d856c36d27ff494ea2854e5f75eebf7da57
TAHTAH Hussein <EXT_HTA37@open-groupe.com>
2020-02-10 11:11:43 +0100

    committing changes for app props and tests run

--- src/test/java/cleanup/CleanUpServicesTest.java
+++ src/test/java/cleanup/CleanUpServicesTest.java
@@ -3,12 +3,13 @@ package cleanup;
 import fr.gouv.culture.francetransfert.FranceTransfertWorkerStarter;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = FranceTransfertWorkerStarter.class)
 public class CleanUpServicesTest {
