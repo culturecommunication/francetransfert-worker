a79920ea73fa318bc2d8126b75866303175c5c92
kfifat abir <a.kfifat@actongroup.com>
2022-07-15 17:38:09 +0200

    modification

--- src/main/java/fr/gouv/culture/francetransfert/utils/WorkerUtils.java
+++ src/main/java/fr/gouv/culture/francetransfert/utils/WorkerUtils.java
@@ -60,10 +60,40 @@ public class WorkerUtils {
 			throw new WorkerException("error of extraction value");
 		}
 	}
+	
+	public static String getFormatFileSizeLanguage(Locale language, double size) {
+		if (language.getLanguage().equals("en")){
+		return getFormattedFileSizeEn (size);
+		}
+		else
+		{
+			return getFormattedFileSize (size);
+		}
+	}
 
 	// convert bit to octets", "Ko", "Mo", "Go", "To
 	public static String getFormattedFileSize(double size) {
 		String[] suffixes = new String[] { "octets", "Ko", "Mo", "Go", "To" };
+
+		double tmpSize = size;
+		int i = 0;
+
+		while (tmpSize >= 1024) {
+			tmpSize /= 1024.0;
+			i++;
+		}
+
+		// arrondi à 10^-2
+		tmpSize *= 100;
+		tmpSize = (long) (tmpSize + 0.5);
+		tmpSize /= 100;
+
+		return tmpSize + " " + suffixes[i];
+	}
+	
+	// convert bit to octets", "Ko", "Mo", "Go", "To
+	public static String getFormattedFileSizeEn(double size) {
+		String[] suffixes = new String[] { "B", "KB", "MB", "GB", "TB" };
 
 		double tmpSize = size;
 		int i = 0;
